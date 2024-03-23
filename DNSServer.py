560a5de9e778fa8401ee2b3a0b8388f358e9d6ff
hayl almolyki <haylalmolyki@gmail.com>
2024-03-23 23:34:03 +0300

    add

--- DNSServer.py
+++ DNSServer.py
@@ -44,7 +44,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 salt = b'Tandon'  # Remember it should be a byte-object
-password = 'your_NYU_email_address'  # Replace with your NYU email address registered in Gradescope
+password = 'ha3001@nyu.edu'  # Replace with your NYU email address registered in Gradescope
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt)  # exfil function
